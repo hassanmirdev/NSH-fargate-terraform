4ee4eab0afc26803bcda627714b49aa8aa552ddf
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-04 19:27:00 +0530

    Update main.tf

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -35,6 +35,8 @@ resource "aws_ecs_task_definition" "appointment_service" {
         }
       }
     }, {
+
+
       # X-Ray Daemon sidecar container definition
       name      = "xray-daemon"
       image     = "amazon/aws-xray-daemon"
@@ -47,7 +49,30 @@ resource "aws_ecs_task_definition" "appointment_service" {
           hostPort      = 2000
         }
       ]
-    }])
+     environment = [
+       
+       name  = "AWS_XRAY_DAEMON_ADDRESS"
+       value = "xray.us-east-1.amazonaws.com:2000"
+     },{
+        name  = "AWS_XRAY_TRACING_NAME"
+        value = "appointment-service-trace"
+       },
+      {
+     {
+      name  = "AWS_XRAY_DAEMON_DISABLE_METADATA"
+      value = "true"
+     }
+     ]
+    
+    logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.ecs_logs.name
+          awslogs-region        = "us-west-2"
+          awslogs-stream-prefix = "xray"
+        }
+      }
+}])
 
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
@@ -102,7 +127,30 @@ resource "aws_ecs_task_definition" "patient_service" {
           hostPort      = 2000
         }
       ]
-    }])
+     environment = [
+       
+       name  = "AWS_XRAY_DAEMON_ADDRESS"
+       value = "xray.us-east-1.amazonaws.com:2000"
+     },{
+        name  = "AWS_XRAY_TRACING_NAME"
+        value = "patient-service-trace"
+       },
+      {
+     {
+      name  = "AWS_XRAY_DAEMON_DISABLE_METADATA"
+      value = "true"
+     }
+     ]
+    
+    logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.ecs_logs.name
+          awslogs-region        = "us-west-2"
+          awslogs-stream-prefix = "xray"
+        }
+      }
+}])
 
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
