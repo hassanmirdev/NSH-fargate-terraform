0d1ea8829ada7afcc10e4e7762d2d12262e050d4
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-06 21:57:04 +0530

    Update main.tf

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -109,6 +109,24 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   })
 }
 
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecs-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Effect = "Allow"
+        Sid     = ""
+      }
+    ]
+  })
+}
+
 resource "aws_iam_policy" "ecs_cloudwatch_policy" {
   name = "ecs-cloudwatch-logs-policy"
   
